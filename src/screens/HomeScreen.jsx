24f69f236e5a0d4a14e71c6d955e505c6ed79623
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-05 18:41:11 +0530

    frontend connected with backend -product screen pending

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import {Col, Row} from 'react-bootstrap'
-import products from "../products"
+// import products from "../products"
 import Product from '../components/Product';
+import axios from 'axios';
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const res =  await axios.get("/api/products")
+            setProducts(res.data)
+        }
+        
+        getProducts()
+    }, []);
+
     return (
         <>
             <h2 className="py-3">Latest Products</h2>
