2ec96d047b652eacbbd58ef92cf06798f988a206
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-06 01:21:13 +0530

    carousel and related images commented

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,7 +7,7 @@ const Product = ({ productContent }) => {
     return (
         <Card className="my-3 rounded">
             <Link to={`/product/${productContent._id}`}>
-                <Card.Img src={productContent.image[1]} variant="top"/>
+                <Card.Img src={productContent.image} variant="top"/>
             </Link>
             <Card.Body>
                 <Link to={`/product/${productContent._id}`}>
--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import {Row, Col, ListGroup, Image, Card, Button, ListGroupItem, Carousel} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios';
 
 
 const ProductScreen = ({ match }) => {
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+    const [loading, setLoading] = useState(false);
+    // const product = products.find(p => p._id === match.params.id)
+    useEffect(() => {
+        const getProduct = async () => {
+            try {
+                setLoading(true);
+                const res =  await axios.get(`/api/products/${match.params.id}`)
+                setProduct(res.data)
+            } 
+            catch (err) {
+                throw err
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        
+        getProduct()
+    }, [])
+
     return (
         <>
                 <Link to="/" className="btn btn-outline-secondary my-3"><i class="fas fa-chevron-left"></i></Link>
                 <Row>
                     <Col md={5} className="p-5">
-                        <Carousel>
-                        {
-                            product.image.map(i => (
-                                <Carousel.Item>
-                                <Image src={i} alt={product.name} ClassName="img-fluid" fluid></Image>
-                            </Carousel.Item>
-                            ))
-                        }
-                            {/* <Carousel.Item>
-                                <Image src={product.image[0]} alt={product.name} ClassName="img-fluid" fluid></Image>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={product.image[1]} alt={product.name} ClassName="img-fluid" fluid></Image>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={product.image[2]} alt={product.name} ClassName="img-fluid" fluid></Image>
-                            </Carousel.Item> */}
-                        </Carousel>
+                    <Image  src={product.image} alt={product.name} ClassName="img-fluid" fluid></Image>
                     </Col>
                     <Col md={4}>
                         <ListGroup variant="flush">
@@ -76,3 +80,15 @@ const ProductScreen = ({ match }) => {
 }
 
 export default ProductScreen
+
+
+
+                        // <Carousel>
+                        // {
+                        //     product.image.map((i, index) => (
+                        //         <Carousel.Item>
+                        //             <Image key={index} src={i} alt={product.name} ClassName="img-fluid" fluid></Image>
+                        //         </Carousel.Item>
+                        //     ))
+                        // }
+                        // </Carousel>
