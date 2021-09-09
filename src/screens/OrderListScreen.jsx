2898f93fd40bd3702502e5044dec6c302939611a
Bhavya Wahie <bhavyawahie@gmail.com>
2021-09-10 03:41:24 +0530

    erroneous isPaidAt changed to paidAt

--- src/screens/OrderListScreen.jsx
+++ src/screens/OrderListScreen.jsx
@@ -45,7 +45,7 @@ const OrderListScreen = ({history}) => {
                                 <td>{order.createdAt.substring(0, 10)}</td>
                                 <td>₹ {order.totalPrice}</td>
                                 <td>
-                                    {order.isPaid ? (order.isPaidAt.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}
+                                    {order.isPaid ? (order.paidAt.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}
                                 </td>
                                 <td>
                                     {order.isDelivered ? (order.isDeliveredAt.substring(0, 10)) : (<i className='fas fa-times' style={{color: 'red'}}></i>)}
