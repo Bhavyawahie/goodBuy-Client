34c35f3639827324c82324670a8eb5935048d717
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-30 01:36:43 +0530

    Link changed to linkcontainer

--- src/components/ProductListItems.jsx
+++ src/components/ProductListItems.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
-// import { Link } from "react-router-bootstrap";
 import Rating from "./Rating";
-import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { LinkContainer } from "react-router-bootstrap";
 
 const ProductListItems = ({ product }) => {
 	const location = useLocation();
 	return(
             <Card className="my-1 mx-0 rounded d-flex flex-row product-card align-items-center py-3">
                 <Col xs={5}>
-                    <Link to={`/product/${product._id}`}>
+                    <LinkContainer to={`/product/${product._id}`}>
                         <Card.Img src={product.image} variant="top"/>
-                    </Link>
+                    </LinkContainer>
                 </Col>
                 <Col>
                 <Card.Body>
-                    <Link to={`/product/${product._id}`}>
+                    <LinkContainer to={`/product/${product._id}`}>
                         <Card.Title as="div" className='d-flex'>
                             <span class="d-inline-block text-truncate" style={{maxWidth: "150px"}}>{product.name}</span>
                         </Card.Title>
-                    </Link>
+                    </LinkContainer>
                     <Card.Text as="div">
                         <Rating value={product.rating} text={location.pathname === "/" ? "" : `${product.numReviews} reviews`}/>
                     </Card.Text>
