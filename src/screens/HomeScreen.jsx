50b28eea9beeccef2243d56a67400fbea45767da
Bhavya Wahie <bhavyawahie@gmail.com>
2023-08-02 20:24:53 +0530

    linter error ignored

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -42,6 +42,7 @@ const HomeScreen = ({ match }) => {
 		return () => {
 			window.removeEventListener("resize", handleResize);
 		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	useEffect(() => {
